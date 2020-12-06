2c3cea87bc8efe8f9a2d33e830514b1748c89312
Tong Chen <tong.chen@roblox.com>
2020-12-05 23:48:00 -0800

    change

--- handler.py
+++ handler.py
@@ -7,7 +7,8 @@ from emailHandler import sendEmail
 def hello(event, context):
     s3 = boto3.resource('s3')
     s3Obj = s3.Object('apartment-search', 'apartmentList.json')
-    apartmentUrl = ['https://www.on-site.com/web/online_app/choose_unit?goal=6&attr=x20&property_id=286650&lease_id=0&unit_id=0&required=']
+    apartmentUrl = ['https://www.on-site.com/web/online_app/choose_unit?goal=6&attr=x20&property_id=286650&lease_id=0&unit_id=0&required=',
+    ]
 
     page = requests.get(apartmentUrl[0])
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -21,4 +22,4 @@ def hello(event, context):
         apartmentList[apt['data-apartment-number']] = None
 
     s3Obj.put(Body=(bytes(json.dumps(apartmentList).encode('UTF-8'))))
-    return
+    return 'Success'
